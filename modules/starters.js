11ff94cbadac8bbea77379bcf76cbdc04cbe1399
Stanislav Gurin <sgurin@justcoded.co>
2017-04-07 16:37:48 +0300

    Remove trash functions from starter

--- modules/starters.js
+++ modules/starters.js
@@ -1,93 +1,69 @@
 var inquirer = require('inquirer'),
-  colors = require('colors'),
-  shell = require('shelljs/global');
+    colors = require('colors'),
+    shell = require('shelljs/global');
 
 colors.setTheme({
-  info: ['white', 'bgBlack'],
-  success: ['green', 'bgBlack'],
-  warn: ['orange', 'bgBlack'],
-  error: ['red', 'bgBlack']
+    info: ['white', 'bgBlack'],
+    success: ['green', 'bgBlack'],
+    warn: ['orange', 'bgBlack'],
+    error: ['red', 'bgBlack']
 });
 
 function logComplete() {
-  console.log('[Complete]'.success);
-  console.log('\n\n');
+    console.log('[Complete]'.success);
+    console.log('\n');
 }
 
 module.exports = function() {
-  var qTypes = [{
-      message: 'Do you need markup starter?',
-      type: 'list',
-      name: 'isMarkup',
-      choices: [{
-        name: 'Yes',
-        value: true
-      }, {
-        name: 'No',
-        value: false
-      }]
-    }, {
-      message: 'Type new project repository url (Default: no repository)',
-      type: 'input',
-      name: 'repo',
-      when: function(answers) {
-        return !answers.isMarkup;
-      }
-    }, {
-      message: function(answers) {
-        return 'Type your branch (default: master)';
-      },
-      when: function(answers) {
-        return answers.repo;
-      },
-      type: 'input',
-      name: 'branch'
+    var qTypes = [{
+        message: '\n********************************************\n\n* Welcome to JustCoded Starter. *\n\n********************************************\n \n Select project type:',
+        type: 'list',
+        name: 'isMarkup',
+        choices: [{
+            name: 'Markup',
+            value: true
+        }, {
+            name: 'JS',
+            value: false
+        }]
     }
+    ];
 
-  ];
+    var prompt = inquirer.createPromptModule();
 
-  var prompt = inquirer.createPromptModule();
+    prompt(qTypes).then(function (answers) {
+        commands(answers)
+    });
 
-  // inquirer.prompt(qTypes, commands);
-  prompt(qTypes).then(function (answers) {
-    commands(answers)
-});
+    function commands(info) {
+        console.log(info);
+        info.type = {
+            repo: 'https://github.com/justcoded/web-starter-kit.git',
+            branch: 'master',
+            exec: function() {
+                console.log('Installing Npm dependencies'.info);
+                exec('npm install');
+                logComplete();
+            }
+        };
 
-  function commands(info) {
-    console.log(info);
-    info.type = {
-        repo: 'https://github.com/justcoded/web-starter-kit.git',
-        branch: 'master',
-        exec: function() {
-          console.log('Installing Npm and Bower dependencies'.info);
-          exec('npm install');
-          logComplete();
+        if (!info.type) {
+            console.log('Still in maintenance, sorry :('.red);
+            return;
         }
-    };
-
-    if (!info.type) {
-      console.log('Still in maintenance, sorry :('.red);
-      return;
+        if (info.isMarkup) {
+            exec('mkdir markup');
+            cd('markup');
+        }
+        if (!info.isMarkup) {
+            console.log('In maintenance, sorry :('.red);
+            return;
+        }
+        console.log('Getting starter files'.info);
+        exec('git clone ' + info.type.repo + ' ./');
+        logComplete();
+        exec('rm -rf .git');
+        info.type.exec && info.type.exec();
+        console.log('Starter was successfully installed. Good luck :) JustCoded'.success);
     }
-    if (info.isMarkup) {
-      exec('mkdir markup');
-      cd('markup');
-    }
-    console.log('Getting starter files'.info);
-    exec('git clone ' + info.type.repo + ' ./');
-    logComplete();
-    exec('rm -rf .git');
-    if (info.repo) {
-      info.branch = info.branch || 'master';
-      console.log('Create initial commit and push it into repository'.info);
-      exec('git init && git add . && git commit -m "Init Starter Kit"');
-      logComplete();
-      exec('git remote add origin ' + info.repo);
-      console.log('Pushing the code to the repository'.info);
-      exec('git push origin ' + info.branch);
-      logComplete();
-    }
-    info.type.exec && info.type.exec();
-    console.log('Starter was successfully installed. Good luck :)'.success);
-  }
 };

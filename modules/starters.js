16e26f93f03501861c3ac17ca791334153904d6e
Kirill Poddubnyi <kpoddubnyi@justcoded.co>
2017-05-08 16:46:10 +0300

    Added config array. Create deleteFiles function. Add Bootstrap question

--- modules/starters.js
+++ modules/starters.js
@@ -2,63 +2,105 @@
 
 const lib = require('../lib/library'),
   inquirer = require('inquirer'),
-  noEmoji = /^win/.test(process.platform);
+  noEmoji = /^win/.test(process.platform),
+  defaultGit = 'https://github.com/justcoded/web-starter-kit.git';
+
+let config = [];
 
 if (noEmoji) {
   emoji.get = () => '';
 }
 
-module.exports = function () {
-  let qTypes = [{
-    message: 'Select project type:',
-    type: 'list',
-    name: 'value',
-    choices: [{
-      name: 'Markup',
-      // Branch name
-      value: true
-    }, {
-      name: 'JavaScript',
-      // Branch name
-      value: false
-    }]
-  },
-  {
-    when: function (response) {
-      return response.value;
-    },
-    message: 'Would you like to use SCSS maps?',
-    type: 'list',
-    name: 'value',
-    choices: [{
-      name: 'Yes',
-      // Branch name
-      value: 'master'
-    }, {
-      name: 'No',
-      // Branch name
-      value: 'Without-SCSS-Map'
-    }]
-  },
-  ];
-
+module.exports = () => {
   console.log('\n*****************************************\n*\tWelcome to JustCoded Starter\t*\n*****************************************\n'.green);
 
-  let prompt = inquirer.createPromptModule();
+  function projectType() {
+    return new Promise(function (resolve, reject) {
+      let questions = [{
+        message: 'Select project type:',
+        type: 'list',
+        name: 'value',
+        choices: [{
+          name: 'Gulp',
+          value: 'Gulp'
+        }, {
+          name: 'Webpack',
+          value: 'Webpack'
+        }]
+      }];
 
-  prompt(qTypes).then((answers) => {
-    commands(answers);
-  });
-
-  function commands(info) {
-    if (!info.value) {
-      console.log('In maintenance, sorry '.red + emoji.get('hourglass'));
-      return;
-    }
-
-    lib.init({
-      url: 'https://github.com/justcoded/web-starter-kit.git',
-      branch: info.value
+      inquirer.prompt(questions).then(answers => {
+        console.log(answers.value);
+        switch (answers.value) {
+          case 'Gulp':
+            // Gulp git
+            config.push({
+              url: defaultGit,
+              branch: 'master'
+            });
+            break;
+          case 'Webpack':
+            console.log('In maintenance, sorry '.red + emoji.get('hourglass'));
+            return;
+          default:
+            console.log('Something went wrong!'.red);
+        }
+        resolve();
+      });
     });
   }
+
+  projectType().then(() => {
+    let questions = [{
+        message: 'Would you like to use SCSS maps?',
+        type: 'list',
+        name: 'value',
+        choices: [{
+          name: 'Yes',
+          value: 'default'
+        }, {
+          name: 'No',
+          value: 'Without-SCSS-Map'
+        }]
+      },
+      {
+        when: function (answers) {
+          if (answers.value === 'Without-SCSS-Map') {
+            config.push({
+              // Without-SCSS-Map git branch
+              url: defaultGit,
+              branch: 'Without-SCSS-Map'
+            });
+          }
+          return answers.value;
+        },
+        message: 'Would you like to use Bootstrap?',
+        type: 'list',
+        name: 'value',
+        choices: [{
+          name: 'Yes',
+          // With-Bootstrap git branch
+          value: 'With-Bootstrap',
+          delete: [
+            'src/scss/abstracts',
+            'src/scss/base'
+          ]
+        }, {
+          name: 'No',
+          value: 'default'
+        }]
+      }
+    ];
+
+    inquirer.prompt(questions).then(answers => {
+      if (answers.value !== 'default') {
+        config.push({
+          url: defaultGit,
+          branch: answers.value
+        });
+      }
+
+      lib.init(config);
+    });
+  });
 };

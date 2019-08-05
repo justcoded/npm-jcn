ccf2a5eb6fdcb541381f77c5490b9ee3b769d78d
Vladimir Pikul <vpikul@justcoded.co>
2019-08-05 14:15:59 +0300

    constructor updated

--- modules/questions.js
+++ modules/questions.js
@@ -1,13 +1,10 @@
 'use strict';
 
-const behavior = require('./behavior'),
-  inquirer = require('inquirer'),
+const inquirer = require('inquirer'),
   isWindows = /^win/.test(process.platform),
   defaultGit = 'https://github.com/justcoded/web-starter-kit.git';
 
 let config = [];
-let isWPforPug = '';
-let isWpGulp = '';
 
 module.exports = () => {
   // Check the jcn version (currently doesn't work on Windows)
@@ -43,7 +40,7 @@ module.exports = () => {
         }]
       }];
 
-      
+
 
       inquirer.prompt(questions).then(answers => {
 
@@ -54,8 +51,6 @@ module.exports = () => {
               url: defaultGit,
               branch: 'master' // Gulp branch
             });
-            
-            isWPforPug = 'With-Pug';
 
             break;
           case 'WordPress':
@@ -63,9 +58,6 @@ module.exports = () => {
               url: defaultGit,
               branch: 'WordPress-Gulp' // Gulp+WP branch
             });
-
-            isWpGulp = 'WordPress-Gulp-';
-            isWPforPug = 'WordPress-With-Pug';
 
             break;
           default:
@@ -77,101 +69,4 @@ module.exports = () => {
     });
   }
 
-  projectType()
-    .then(() => {
-      let questions = [{
-          message: 'Would you like to use Pug?',
-          type: 'list',
-          name: 'value',
-          choices: [{
-            name: 'Yes',
-            // With-Pug git branch
-            value: 'With-Pug'
-          }, {
-            name: 'No',
-            value: 'Markup-Gulp'
-          }],
-        },
-        {
-          when: function (answers) {
-            if (answers.value === 'With-Pug') {
-              // Push the additional information to config
-              config.push({
-                url: defaultGit,
-                branch: isWPforPug
-              });
-            }
-
-            return true;
-          },
-          message: 'Would you like to use CSS Framework?',
-          type: 'list',
-          name: 'value',
-          choices: [{
-            name: 'Yes',
-            value: 'CssFramework-Yes'
-          }, {
-            name: 'No',
-            value: 'CssFramework-No'
-          }],
-        },
-        {
-          when: function (answers) {
-            return answers.value === 'CssFramework-No' ? true : false;
-          },
-          // This question will appear only if user doesn't want to use CSS Frameworks
-          message: 'Would you like to use SCSS maps?',
-          type: 'list',
-          name: 'value',
-          choices: [{
-            name: 'Yes',
-            value: 'default'
-          }, {
-            name: 'No',
-            value: 'Without-SCSS-Map'
-          }]
-        },
-        {
-          when: function (answers) {
-            return answers.value === 'CssFramework-Yes' ? true : false;
-          },
-          // This question will appear onlu if user wantsto use CSS Frameworks
-          message: 'Which Framework do you want to use?',
-          type: 'list',
-          name: 'value',
-          choices: [{
-            name: 'Bootstrap',
-            // With-Bootstrap git branch
-            value: 'With-Bootstrap'
-          }, {
-            name: 'Materialize',
-            // With-Materialize git branch
-            value: 'With-Materialize'
-          }]
-        }
-      ];
-
-      inquirer.prompt(questions)
-        .then(answers => {
-          switch (answers.value) {
-            case 'Without-SCSS-Map':
-            case 'With-Bootstrap':
-            case 'With-Materialize':
-              // Push the additional information to config
-              config.push({
-                url: defaultGit,
-                branch: isWpGulp + answers.value
-              });
-              break;
-          }
-
-          behavior.init(config, isWPforPug);
-        })
-        .catch(e => {
-          console.log(e);
-        });
-    })
-    .catch(e => {
-      console.log(e);
-    });;
 };
